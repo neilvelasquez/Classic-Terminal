791749c7b1465f664dc397e67261871fb9548cbd
DevNeilV4 <55757326+neilvelasquez@users.noreply.github.com>
2020-02-05 19:56:00 -0800

    Add files via upload

---
+++ WM2000/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() 
+    {
+        ShowMainMenu("Hello Dude");
+
+    }
+
+    void ShowMainMenu(string greeting) { 
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Where the Whitegirls at?");
+        Terminal.WriteLine("Press 1 for local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter Yourselection");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "1")
+        {
+            print("They are at Campbell");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
